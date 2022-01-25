7b8fca490f5b257c3ee7e1060c21d0aa5e2f0c2c
Efkan Turedi <turedi.efkan@gmail.com>
2022-01-26 00:47:32 +0100

    Added utils

--- 07_research_yolo/utils.py
+++ 07_research_yolo/utils.py
@@ -1,6 +1,19 @@
-import os 
+import os
+from sklearn.dummy import DummyClassifier 
 
 def total_detection_counter():
+    """
+    Raison d'être:
+    This function that does return the number of detection in a given folder 
+    which contains either training set or test set
+
+    Args: 
+    None
+
+    Returns:
+    None. Just prints the numbers of truth detections in the folder
+    """
+
     num_lines = 0
     for filename in os.listdir('.'):
         # Checking if filename ends with '.txt'
@@ -13,6 +26,19 @@ def total_detection_counter():
 total_detection_counter()
 
 def total_masked_counter():
+    """
+    Raison d'être:
+    This function that does return the number of truth masked detection in a given folder 
+    which contains either training set or test set
+    
+    Args:
+    None
+
+    Returns:
+    None. Just prints the numbers of truth masked detections in the folder
+    
+    """
+
     num_line_sec = 0
     for filename in os.listdir('.'):
         # Checking if filename ends with '.txt'
@@ -25,3 +51,38 @@ def total_masked_counter():
     print(num_line_sec)
 
 total_masked_counter()
+
+def get_list_detections():
+"""
+    Raison d'être:
+    This function is built to determine the class of each truth detection in the dataset
+    
+    Args:
+    None
+
+    Returns:
+    List of truth detections ranked by class 
+"""
+    num_line_sec = 0
+    results = []
+    for filename in os.listdir('.'):
+        # Checking if filename ends with '.txt'
+        if filename.endswith(".txt"):
+            sub_file = open(filename,"r")
+            for line in sub_file:
+                results.append(line[0])
+            sub_file.close()
+    print(results)
+
+get_list_detections()
+
+def dummy_counter():
+    """
+
+
+
+    """
+    dummy = DummyClassifier(strategy='most_frequent')
+
+
+    
